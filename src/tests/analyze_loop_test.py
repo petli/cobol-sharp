fc5cc7544e6f1d7f83e8a1348a9cf7bf1a84ef1f
Peter Liljenberg <peter.liljenberg@gmail.com>
2016-10-24 19:26:39 +0200

    Unit test for testing breaking from inner loops.

--- src/tests/analyze_loop_test.py
+++ src/tests/analyze_loop_test.py
@@ -52,7 +52,7 @@ def test_return_in_loop(cobol_block):
     ).assert_block(cobol_block)
 
 
-def test_break_from_loop(cobol_block, cobol_debug):
+def test_break_from_loop(cobol_block):
     """
        loop.
          perform a.
@@ -82,4 +82,68 @@ def test_break_from_loop(cobol_block, cobol_debug):
                False),
             PerformSectionStatement(None, None, 'b'))),
         PerformSectionStatement(None, None, 'c'),
+    ).assert_block(cobol_block)
+
+
+def test_break_from_inner_loop(cobol_block):
+    """
+       outer-loop.
+         perform outer-a.
+
+       inner-loop.
+         perform inner-a.
+
+         if x > y
+             go to finish-inner.
+         if x > z
+             go to finish-inner.
+
+         if error = 1
+             go to finish-outer.
+
+         perform inner-b.
+         go to inner-loop.
+
+       finish-inner.
+         if error = 1
+             go to finish-outer.
+
+         perform outer-b.
+         go to outer-loop.
+
+       finish-outer.
+         perform c.
+         exit.
+"""
+    finish_outer_label = GotoLabel('finish-outer', None)
+
+    ExpectedBlock(
+        Forever(None, ExpectedBlock(
+            PerformSectionStatement(None, None, 'outer-a'),
+
+            Forever(None, ExpectedBlock(
+                PerformSectionStatement(None, None, 'inner-a'),
+                If(None,
+                   ExpectedBlock(Break()),
+                   ExpectedBlock(),
+                   False),
+                If(None,
+                   ExpectedBlock(Break()),
+                   ExpectedBlock(),
+                   False),
+                If(None,
+                   ExpectedBlock(Goto(finish_outer_label)),
+                   ExpectedBlock(),
+                   False),
+                PerformSectionStatement(None, None, 'inner-b'))),
+
+            If(None,
+               ExpectedBlock(Goto(finish_outer_label)),
+               ExpectedBlock(),
+               False),
+            PerformSectionStatement(None, None, 'outer-b'))),
+
+        finish_outer_label,
+        PerformSectionStatement(None, None, 'c'),
+        Return(),
     ).assert_block(cobol_block)
